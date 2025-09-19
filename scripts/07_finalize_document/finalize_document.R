89336579b401f73525c056231ab27288159cc5ee
jacobdum <jacob@a2-ai.com>
2025-09-19 16:52:21 +0000

    update 07

--- scripts/07_finalize_document/finalize_document.R
+++ scripts/07_finalize_document/finalize_document.R
@@ -1,13 +1,35 @@
+# ------------------------------------------------------------------------------
+# Package installation
+# ------------------------------------------------------------------------------
 install.packages("pak")
 install.packages("here")
 
 pak::pkg_install("a2-ai/reportifyr")
 
+# ------------------------------------------------------------------------------
+# Load reportifyr
+# ------------------------------------------------------------------------------
 library(reportifyr)
 
-initialize_report_project(project_dir = here::here())
+# ------------------------------------------------------------------------------
+# Initialize report project
+# ------------------------------------------------------------------------------
+initialize_report_project(
+  project_dir = here::here(),
+  report_dir_name = NULL,
+  outputs_dir_name = NULL
+)
 
+# ------------------------------------------------------------------------------
+# Set paths
+# ------------------------------------------------------------------------------
+config <- here::here("report", "config.yaml")
+
+# ------------------------------------------------------------------------------
+# Finalize document
+# ------------------------------------------------------------------------------
 finalize_document(
-  docx_in = here::here("report", "draft", "template-draft.docx"), ## Using prepared draft from 06_build_report
-  docx_out = here::here("report", "final", "template-final.docx")
+  docx_in = here::here("report", "draft", "template-draft.docx"),
+  docx_out = here::here("report", "final", "template-final.docx"),
+  config_yaml = config
 )
